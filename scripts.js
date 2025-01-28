e42ec618cc16ec6a3d1f301c256dfd6ecd3ff553
Bunchhieng <Bunchhieng@gmail.com>
2025-01-28 18:01:23 -0500

    updated

--- scripts.js
+++ scripts.js
@@ -1119,7 +1119,7 @@ window.addEventListener('load', () => DraftManager.init());
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('./service-worker.js')
       .then(registration => console.log('ServiceWorker registered'))
       .catch(err => console.log('ServiceWorker registration failed:', err));
   });
--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
 const CACHE_NAME = 'soccer-pickup-draft-v1';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png',
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
   'https://html2canvas.hertzen.com/dist/html2canvas.min.js',
   'https://fonts.googleapis.com/css2?family=Geist:wght@300;400;500;600;700&display=swap'
 ];
